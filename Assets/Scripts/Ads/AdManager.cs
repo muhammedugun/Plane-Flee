d8102064f6d41da46c570cc5ef8792dff85bb609
muhammedugun <0muhammed.ugun@gmail.com>
2023-05-26 11:24:03 +0300

    1.0.0
    Release Sürümü

---
+++ Assets/Scripts/Ads/AdManager.cs
@@ -0,0 +1,67 @@
+﻿using GoogleMobileAds.Api;
+using UnityEngine;
+
+namespace Assets.Scripts.Ads
+{
+    public class AdManager : MonoBehaviour
+    {
+        public int bannerX, bannerY;
+        public static MyRewardedAd rewardedAd;
+        MyBannerAd bannerAd;
+        private static AdManager instance;
+
+        
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+                DontDestroyOnLoad(gameObject);
+                bannerAd = new MyBannerAd();
+                rewardedAd = new MyRewardedAd();
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+            
+        }
+        public void Start()
+        {
+            // When true all events raised by GoogleMobileAds will be raised
+            // on the Unity main thread. The default value is false.
+            MobileAds.RaiseAdEventsOnUnityMainThread = true;
+
+            // Initialize the Google Mobile Ads SDK.
+            //MobileAds.Initialize(initStatus => { });
+
+            // Initialize the Google Mobile Ads SDK.
+            MobileAds.Initialize((InitializationStatus initStatus) =>
+            {
+                
+            });
+
+            RequestConfiguration requestConfiguration = new RequestConfiguration
+            {
+                TagForUnderAgeOfConsent = TagForUnderAgeOfConsent.True,
+                MaxAdContentRating = MaxAdContentRating.PG
+            };
+            MobileAds.SetRequestConfiguration(requestConfiguration);
+
+
+
+            bannerAd.CreateBannerView();
+            bannerAd.ListenToAdEvents();
+            bannerAd.LoadAd();
+            rewardedAd.LoadRewardedAd();
+            var banner = GameObject.Find("BANNER(Clone)");
+            DontDestroyOnLoad(banner);
+
+
+        }
+
+
+        
+
+    }
+}
--- Assets/Scripts/Skin/SkinMenuControl.cs
+++ Assets/Scripts/Skin/SkinMenuControl.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using Assets.Scripts.Ads;
+using TMPro;
 using UnityEngine;
 
 namespace Assets.Scripts.Skin
@@ -8,8 +9,6 @@ namespace Assets.Scripts.Skin
         public GameObject yellowCheckMark, blueCheckMark, greenCheckMark, redCheckMark;
         public GameObject blueLockText, greenLockText, redLockText;
         public GameObject blueTopLockText, greenTopLockText, redTopLockText;
- 
-
 
         void Start()
         {
@@ -37,12 +36,27 @@ namespace Assets.Scripts.Skin
 
             LockTextPointControl("blueLockText", blueLockText, 50);
             LockTextPointControl("greenLockText", greenLockText, 80);
-          
+            LockTextAdControl("redLockText", redLockText);
+
+
             LockTextControl("blueLockText", blueLockText, blueTopLockText);
             LockTextControl("greenLockText", greenLockText, greenTopLockText);
             LockTextControl("redLockText", redLockText, redTopLockText);
         }
-       
+
+
+        void LockTextAdControl(string key, GameObject lockText)
+        {
+            if (!PlayerPrefs.HasKey("adWatched"))
+            {
+                PlayerPrefs.SetInt("adWatched", 0);
+            }
+            if (PlayerPrefs.GetInt("adWatched") == 1)
+            {
+                PlayerPrefs.SetInt(key, 1);
+            }
+        }
+
 
         void LockTextPointControl(string key,GameObject lockText, int unlockScore)
         {
@@ -107,6 +121,15 @@ namespace Assets.Scripts.Skin
         }
         public void ButtonRed()
         {
+            if(PlayerPrefs.GetInt("adWatched") == 0)
+            {
+                PlayerPrefs.SetInt("adWatched",1);
+                AdManager.rewardedAd.ShowRewardedAd();
+                LockTextAdControl("redLockText", redLockText);
+                LockTextControl("redLockText", redLockText, redTopLockText);
+            }
+
+
             if ((PlayerPrefs.GetInt("redLockText") == 1))
             {
                 PlayerPrefs.SetInt("checkMark", 3);
